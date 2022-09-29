78b307f11a8d1278627f2ecf08106f97e226cb76
loiq.amonbekov <amonbekov.loiq@gmail.com>
2022-09-29 14:34:38 +0500

    Added button and table to Category Index Page

---
+++ resources/js/Pages/Admin/Category/Index.vue
@@ -0,0 +1,67 @@
+<template>
+    <Head title="Categories"/>
+    <AuthenticatedLayout>
+        <div class="p-9">
+            <div>
+                <h1>Categories Dashboard</h1>
+            </div>
+            <div class="py-6">
+                <button type="button"
+                        class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    Add New Category
+                </button>
+            </div>
+
+            <div>
+
+                <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-orange-50">
+                        <tr>
+                            <th scope="col" class="py-3 px-6">
+                                ID
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Name
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Description
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                <span class="sr-only">Edit</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr class="bg-white border-b  hover:bg-gray-50">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap ">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="py-4 px-6">
+                                Sliver
+                            </td>
+                            <td class="py-4 px-6">
+                                $2999
+                            </td>
+                            <td class="py-4 px-6 text-right">
+                                <a href="#" class="font-medium text-blue-600 hover:underline">Edit</a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from '@inertiajs/inertia-vue3';
+</script>
+
+<style scoped>
+
+</style>
