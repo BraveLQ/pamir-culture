63ac7733ff27ac85590c7a2a22f649b1a237439b
loiq.amonbekov <amonbekov.loiq@gmail.com>
2023-01-29 16:33:09 +0500

    Public Menu bar was added

---
+++ resources/js/Pages/Public/Navbar/Navbar.vue
@@ -0,0 +1,109 @@
+<template>
+    <nav class=" bg-white w-full flex relative justify-between items-center mx-auto px-8 h-20 font-serif shadow-2xl">
+        <!-- logo -->
+        <div class="inline-flex">
+            <div class="text-2xl">Pamir Culture</div>
+        </div>
+
+        <!-- end logo -->
+
+        <!-- search bar -->
+        <div class="hidden sm:block flex-shrink flex-grow-0 justify-start px-2">
+            <div class="inline-block">
+                <div class="inline-flex items-center max-w-full">
+                    <input class="flex items-center pl-2  w-60 border rounded-full px-1 focus:border-gray-600 py-1" type="text">
+
+                    <div class="flex items-center justify-center relative  h-8 w-8 rounded-full">
+                        <svg
+                            viewBox="0 0 32 32"
+                            xmlns="http://www.w3.org/2000/svg"
+                            aria-hidden="true"
+                            focusable="false"
+                            style="
+                                display: block;
+                                fill: none;
+                                height: 12px;
+                                width: 12px;
+                                stroke: currentcolor;
+                                stroke-width: 5.33333;
+                                overflow: visible;
+                            "
+                        >
+                            <g fill="none">
+                                <path
+                                    d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"
+                                ></path>
+                            </g>
+                        </svg>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- end search bar -->
+
+        <!-- login -->
+        <div class="flex-initial">
+            <div class="flex justify-end items-center relative">
+
+<!--                <div class="flex mr-4 items-center hidden md:visible">-->
+<!--                    <a class="inline-block py-2 px-3 hover:bg-gray-200 rounded-full" href="#">-->
+<!--                        <div class="flex items-center relative cursor-pointer whitespace-nowrap">Become a host</div>-->
+<!--                    </a>-->
+<!--                </div>-->
+
+                <div @click="toggleMenu = !toggleMenu" class="block">
+                    <div class="inline relative">
+                        <button type="button" class="inline-flex items-center relative px-2 border rounded-full hover:shadow-lg">
+                            <div class="pl-1">
+                                <img v-if="toggleMenu" class="h-5" src="/icons/i_menu_close.svg">
+                                <img v-else class="h-4" src="/icons/i_menu.svg">
+<!--                                <svg-->
+<!--                                    viewBox="0 0 32 32"-->
+<!--                                    xmlns="http://www.w3.org/2000/svg"-->
+<!--                                    aria-hidden="true"-->
+<!--                                    focusable="false"-->
+<!--                                    style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 3; overflow: visible;"-->
+<!--                                >-->
+<!--                                    <g fill="none" fill-rule="nonzero">-->
+<!--                                        <path d="m2 16h28"></path>-->
+<!--                                        <path d="m2 24h28"></path>-->
+<!--                                        <path d="m2 8h28"></path>-->
+<!--                                    </g>-->
+<!--                                </svg>-->
+                            </div>
+
+                            <div class="block flex-grow-0 flex-shrink-0 h-10 w-12 pl-5">
+                                <svg
+                                    viewBox="0 0 32 32"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true"
+                                    focusable="false"
+                                    style="display: block; height: 100%; width: 100%; fill: currentcolor;"
+                                >
+                                    <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
+                                </svg>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end login -->
+    </nav>
+    <div v-if="toggleMenu" class=" flex  justify-end">
+        <div class="bg-white absolute w-full md:w-48 font-serif shadow-2xl">
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const toggleMenu = ref(false)
+</script>
+
+<style scoped>
+
+</style>
